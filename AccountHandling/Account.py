4b8d3ae1df0b7930deacabe619ad46d962cb3e50
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2019-08-11 13:05:41 -0700

    created Account class

---
+++ AccountHandling/Account.py
@@ -0,0 +1,17 @@
+class Account:
+    # Initializes the Account
+    # apiDict is a dictionary of exchanges and api keys
+    def __init__(self, apiDict):
+        self.apiDict = apiDict
+
+    def start(self):
+        pass
+    
+    def close(self):
+        pass
+
+    def liquidate(self):
+        pass
+
+    
+        
